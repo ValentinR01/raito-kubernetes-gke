cf994445fa34a4de1d9236e05486ddf999886c49
valentin Mariot <valentinmariot@gmail.com>
2022-04-26 16:16:26 +0200

    [Text] - New component w/ style

---
+++ front/src/components/atoms/grouping/Text/Text.style.js
@@ -0,0 +1,27 @@
+import styled, { css } from 'styled-components'
+
+const TextStyle = styled.p`
+    color: blue;
+    ${({ $size }) => {
+        let fontSize;
+        switch ($size) {
+            case 'small':
+                fontSize = 20;
+                break;
+            case 'large':
+                fontSize = 36;
+                break;
+            case 'xlarge':
+                fontSize = 48;
+                break;
+            default:
+                fontSize = 24;
+                break;
+        }
+        return css`
+            font-size: ${fontSize}px;
+        `
+    }}
+`;
+
+export default TextStyle;
