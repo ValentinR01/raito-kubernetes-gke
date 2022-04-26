3f2926322c5c39ea873f5b6cf54ffc23cb9e6b79
valentin Mariot <valentinmariot@gmail.com>
2022-04-26 16:16:26 +0200

    [Text] - New component w/ style

---
+++ front/src/components/atoms/grouping/Text/Text.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+import TextStyle from './Text.style';
+
+const Text = ({
+    className,
+    children,
+    color,
+    size,
+}) => {
+  return (
+    <TextStyle
+        // Component
+        className={className}
+        // Style
+        $color={color}
+        $size={size}
+    >
+        {children}
+    </TextStyle>
+  )
+}
+
+Text.propTypes = {
+  children: PropTypes.string,
+  className: PropTypes.string,
+  size: PropTypes.oneOf([
+    'small',
+    'medium',
+    'large',
+    'xlarge',
+  ]),
+  color: PropTypes.oneOf([
+    'cream',
+    'terra',
+    'primary',
+    'secondary'
+  ]),
+}
+
+Text.defaultProps = {
+  children: null,
+  className: null,
+  size: 'medium',
+  color: 'primary',
+}
+
+export default Text;
