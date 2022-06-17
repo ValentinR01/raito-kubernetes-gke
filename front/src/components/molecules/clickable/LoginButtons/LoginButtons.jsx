a5354668ad0d6ed8de9cea221a29bfb9889e102b
TaniaMAHOUCHE <mahouchetania15@gmail.com>
2022-06-17 17:51:07 +0200

    (Multiple files) - feat: Creating page template

---
+++ front/src/components/molecules/clickable/LoginButtons/LoginButtons.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginButtons = () => {
+  return (
+    <div>LoginButtons</div>
+  )
+}
+
+export default LoginButtons
---
+++ front/src/components/templates/PageTemplate.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SideBar from 'components/organisms/SideBar/SideBar';
+import LoginButtons from 'components/molecules/clickable/LoginButtons/LoginButtons';
+
+const PageTemplate = ({children}) => {
+    return (
+        <div>
+            <SideBar />
+            <LoginButtons />
+            {children}
+        </div>
+    );
+};
+
+export default PageTemplate;
