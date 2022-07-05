66cd35216e336b248981537a88eb251d3123af1e
Ada Loveless <xhorj@protonmail.com>
2022-07-05 15:26:51 +0200

    (PageTemplate) - refactor: linter

--- front/src/components/templates/PageTemplate.jsx
+++ front/src/components/templates/PageTemplate.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import SideBar from 'components/organisms/SideBar/SideBar';
-import LoginButtons from 'components/molecules/clickable/LoginButtons/LoginButtons';
+import React from "react";
+import SideBar from "components/organisms/SideBar/SideBar";
+import LoginButtons from "components/molecules/clickable/LoginButtons/LoginButtons";
 
-const PageTemplate = ({children}) => {
-    return (
-        <div>
-            <SideBar />
-            <LoginButtons />
-            {children}
-        </div>
-    );
+const PageTemplate = ({ children }) => {
+  return (
+    <div>
+      <SideBar />
+      <LoginButtons />
+      {children}
+    </div>
+  );
 };
 
 export default PageTemplate;
