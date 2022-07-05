64001d21dd386934217107b68bef6827606a389e
DFarau <doriane.farau@hetic.net>
2022-07-05 15:11:08 +0200

    (Buttons) - feat: added Buttons

---
+++ front/src/components/molecules/clickable/BasicButton/BasicButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import BasicButtonStyle from './BasicButton.style'
+const BasicButtons = () => {
+  return (
+    <div>
+      <BasicButtonStyle>
+       Se connecter
+      </BasicButtonStyle>
+      <BasicButtonStyle primary>
+       Se connecter
+      </BasicButtonStyle>
+    </div>
+  )
+}
+
+export default BasicButtons
---
+++ front/src/components/molecules/clickable/LoginButtons/LoginButtonsRight.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const LoginButtonsStyleRight = styled.button`
+    background-color: #a44d4d;
+    padding: 0.35em 1em;
+    border: none;
+    border-radius: 0 14px 14px 0;
+        &:hover {
+            background-color: #ce6060;
+        }
+`;
+
+export default LoginButtonsStyleRight;
