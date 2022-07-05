8035d1ff5bf507dd6c18c21e311dfd8a8e7acb7f
Ada Loveless <xhorj@protonmail.com>
2022-07-05 11:21:20 +0200

    (Label.jsx) - Created Label and its style

---
+++ front/src/components/atoms/forms/Label/Label.style.jsx
@@ -0,0 +1,12 @@
+import styled, { css } from "styled-components";
+
+const LabelStyle = styled.label`
+  padding-top: 10px;
+  padding-bottom: 15px;
+  
+  ${({}) => {
+    return css``;
+  }};
+`;
+
+export default LabelStyle;
