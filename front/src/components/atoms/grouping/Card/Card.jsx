e05b272daeea64caf74e7e872b0fc1e4377cb493
Ada Loveless <xhorj@protonmail.com>
2022-07-05 14:23:07 +0200

    (Card) - feat: handle multiple Card sizes

--- front/src/components/atoms/grouping/Card/Card.jsx
+++ front/src/components/atoms/grouping/Card/Card.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import CardStyle from "./Card.style";
 
-const Card = ({ children }) => {
-  return <CardStyle>{children}</CardStyle>;
+const Card = ({ children, small = false, medium = false }) => {
+  return (
+    <CardStyle $small={small} $medium={medium}>
+      {children}
+    </CardStyle>
+  );
 };
 
 export default Card;
--- front/src/components/atoms/grouping/Card/Card.style.js
+++ front/src/components/atoms/grouping/Card/Card.style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const CardStyle = styled.section`
   background-color: #f3edd7;
@@ -7,6 +7,18 @@ const CardStyle = styled.section`
   padding: 10px;
   border-radius: 10px;
   text-align: center;
+
+  ${({ $medium, $small }) => {
+    let width = "1133px";
+    if ($medium) {
+      width = "742px";
+    } else if ($small) {
+      width = "371px";
+    }
+    return css`
+      width: ${width};
+    `;
+  }};
 `;
 
 export default CardStyle;
