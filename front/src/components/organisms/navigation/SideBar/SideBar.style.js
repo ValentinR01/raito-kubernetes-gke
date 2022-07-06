b561fecc698fa4cda422d014a08a15f579438440
Ada Loveless <xhorj@protonmail.com>
2022-07-06 15:28:09 +0200

    (SideBar) - fix: style

--- front/src/components/organisms/navigation/SideBar/SideBar.style.js
+++ front/src/components/organisms/navigation/SideBar/SideBar.style.js
@@ -2,14 +2,12 @@ import styled from 'styled-components';
 
 const SideBarStyle = styled.div`
 background-color: #A44D4D;
-position: relative;
 min-width: 250px;
 height: 95vh;
 border-radius: 12px;
 margin: 10px;
 display:flex;
 flex-direction: column;
-flex: 0.5 0.5 1;
 justify-content:space-between;
 align-items: center;
 `;
