27ad02a6f26f79670fbf34088b138d215a704770
Ada Loveless <xhorj@protonmail.com>
2022-07-06 15:28:43 +0200

    (PageContentTemplate) - feat: created page content template

---
+++ front/src/components/templates/PageContentTemplate/PageContentTemplate.style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const PageContentTemplateStyle = styled.div`
+width: 100vw;
+padding-top: 80px;
+display: flex;
+flex-direction: column;
+align-items:center;
+overflow:auto;
+scrollbar: hidden;
+}
+`;
+
+export default PageContentTemplateStyle;
