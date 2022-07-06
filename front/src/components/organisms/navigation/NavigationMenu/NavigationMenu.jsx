497f1c5903f6a8df0be837dfca8c923efcd56987
Ada Loveless <xhorj@protonmail.com>
2022-07-06 16:32:31 +0200

    refactor: profile icon and props

--- front/src/components/organisms/navigation/NavigationMenu/NavigationMenu.jsx
+++ front/src/components/organisms/navigation/NavigationMenu/NavigationMenu.jsx
@@ -1,28 +1,34 @@
 import React from "react";
 import NavigationMenuStyle from "./NavigationMenu.style";
 import MeditationIcon from "components/atoms/embedding/MeditationIcon/MeditationIcon";
-import ProfilIcone from "assetsSVG-main/ProfilIcone";
+import ProfileIcon from "components/atoms/embedding/ProfileIcon/ProfileIcon";
 import NavigationMenuLink from "components/molecules/navigation/NavigationMenuLink";
 import AmbianceIcon from "components/atoms/embedding/AmbianceIcon/AmbianceIcon";
+import { themeColorLight } from "style/vars/colors";
 
 const NavigationMenu = () => {
   const navMenuData = {
     ambiances: {
       title: "Ambiances",
       path: "/ambiance/",
-      icon: <AmbianceIcon color="#F3EDD7" iconeSize="15" />,
+      icon: <AmbianceIcon />,
     },
     respiration: {
       title: "Respiration",
       path: "/respiration/",
       icon: (
-        <MeditationIcon stroke="none" fill="#F3EDD7" width="15" height="15" />
+        <MeditationIcon
+          stroke="none"
+          fill={themeColorLight.cream}
+          width="15"
+          height="15"
+        />
       ),
     },
     profil: {
       title: "Profil",
       path: "/profil",
-      icon: <ProfilIcone fill={"#F3EDD7"} width="15" height="15" />,
+      icon: <ProfileIcon fill={themeColorLight.cream} width="15" height="15" />,
     },
   };
 
