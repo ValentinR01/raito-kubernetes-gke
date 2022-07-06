b07489ca0307ca76451d8b3f0013d16d5c041982
Ada Loveless <xhorj@protonmail.com>
2022-07-06 16:17:04 +0200

    fix: prettier

--- front/src/components/organisms/navigation/NavigationMenu/NavigationMenu.jsx
+++ front/src/components/organisms/navigation/NavigationMenu/NavigationMenu.jsx
@@ -1,36 +1,50 @@
-import React from 'react'
-import NavigationMenuStyle from './NavigationMenu.style';
-import AmbianceIcone from 'assetsSVG-main/Ambiance';
-import MeditationIcon from 'assetsSVG-main/MeditationIcon';
-import ProfilIcone from 'assetsSVG-main/ProfilIcone';
-import NavigationMenuLink from 'components/molecules/navigation/NavigationMenuLink';
+import React from "react";
+import NavigationMenuStyle from "./NavigationMenu.style";
+import MeditationIcon from "assetsSVG-main/MeditationIcon";
+import ProfilIcone from "assetsSVG-main/ProfilIcone";
+import NavigationMenuLink from "components/molecules/navigation/NavigationMenuLink";
+import AmbianceIcon from "components/atoms/embedding/AmbianceIcon/AmbianceIcon";
 
 const NavigationMenu = () => {
-    const navMenuData = {
-        ambiances: {
-              title: "Ambiances",
-              path: "/ambiance/",
-              icon: < AmbianceIcone color="#F3EDD7" iconeSize="15"/>,    
-            },
-           respiration: {
-              title: "Respiration",
-              path: "/respiration/",
-              icon: < MeditationIcon  stroke="none" fill="#F3EDD7"  width="15" height="15" />,
-            },
-            profil: {
-              title: "Profil",
-              path: "/profil",
-              icon: < ProfilIcone fill={"#F3EDD7"}  width="15" height="15" />,
-            }
-        }
-      
+  const navMenuData = {
+    ambiances: {
+      title: "Ambiances",
+      path: "/ambiance/",
+      icon: <AmbianceIcon color="#F3EDD7" iconeSize="15" />,
+    },
+    respiration: {
+      title: "Respiration",
+      path: "/respiration/",
+      icon: (
+        <MeditationIcon stroke="none" fill="#F3EDD7" width="15" height="15" />
+      ),
+    },
+    profil: {
+      title: "Profil",
+      path: "/profil",
+      icon: <ProfilIcone fill={"#F3EDD7"} width="15" height="15" />,
+    },
+  };
+
   return (
     <NavigationMenuStyle className="side-navbar-menu">
-        <NavigationMenuLink path={navMenuData.ambiances.path} icon={navMenuData.ambiances.icon} title={navMenuData.ambiances.title}/>
-        <NavigationMenuLink path={navMenuData.respiration.path} icon={navMenuData.respiration.icon} title={navMenuData.respiration.title}/>
-        <NavigationMenuLink path={navMenuData.profil.path} icon={navMenuData.profil.icon} title={navMenuData.ambiances.title}/>
+      <NavigationMenuLink
+        path={navMenuData.ambiances.path}
+        icon={navMenuData.ambiances.icon}
+        title={navMenuData.ambiances.title}
+      />
+      <NavigationMenuLink
+        path={navMenuData.respiration.path}
+        icon={navMenuData.respiration.icon}
+        title={navMenuData.respiration.title}
+      />
+      <NavigationMenuLink
+        path={navMenuData.profil.path}
+        icon={navMenuData.profil.icon}
+        title={navMenuData.ambiances.title}
+      />
     </NavigationMenuStyle>
-  )
-}
+  );
+};
 
-export default NavigationMenu
+export default NavigationMenu;
