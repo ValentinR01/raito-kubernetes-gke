1fe6825aab1e7ad2407b6fd28429114382fba228
Ada Loveless <xhorj@protonmail.com>
2022-07-06 15:45:09 +0200

    (SideBar) - fix: renamed icons

--- front/src/components/organisms/navigation/SideBar/SideBar.jsx
+++ front/src/components/organisms/navigation/SideBar/SideBar.jsx
@@ -1,5 +1,5 @@
-import ArchIcone from 'assetsSVG-main/Arch';
-import LogoIcone from 'assetsSVG-main/Logo';
+import ToriIcon from 'assetsSVG-main/Arch';
+import LogoIcon from 'assetsSVG-main/Logo';
 import React from 'react'  ; 
 import "./Sidebar.css" ;
 import NavigationMenu from 'components/organisms/navigation/NavigationMenu/NavigationMenu';
@@ -10,9 +10,9 @@ const SideBar = () => {
 
   return (
     <SideBarStyle>
-        <LogoIcone  fill="#F3EDD7"  width="78" height="78" />
+        <LogoIcon fill="#F3EDD7"  width="78" height="78" />
         <NavigationMenu/>
-        <ArchIcone  fill="#F3EDD7" stroke="#F3EDD7" width="70" height="70" />
+        <ToriIcon  fill="#F3EDD7" stroke="#F3EDD7" width="70" height="70" />
     </SideBarStyle>
   )
 }
