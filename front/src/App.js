320439739b828975dcaa83602cd7fea86b51fc0e
Ada Loveless <xhorj@protonmail.com>
2022-07-06 16:38:07 +0200

    (App) - fix: formatting and added app Background

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,21 @@
 import "App.css";
-import React from 'react';
+import React from "react";
 import ThemeProvider from "providers/ThemeProvider";
 import { Routes, Route } from "react-router-dom";
-import AppBackground from "components/atoms/embeding/AppBackground/AppBackground";
+import AppBackground from "components/molecules/embedding/AppBackground/AppBackground";
 
 function App() {
-
   return (
     <ThemeProvider>
       <div className="App">
         <Routes>
-          <Route path="/" element={ <div />} />
-          <Route path="/respiration/" element={ < div />}  />
-          <Route path="/ambiance/" element={ < div /> } />
-          <Route path="/profil" element={ < div /> } />
-        </Routes> 
-        <AppBackground/>
-    </div>
+          <Route path="/" element={<div />} />
+          <Route path="/respiration/" element={<div />} />
+          <Route path="/ambiance/" element={<div />} />
+          <Route path="/profil" element={<div />} />
+        </Routes>
+        <AppBackground />
+      </div>
     </ThemeProvider>
   );
 }
