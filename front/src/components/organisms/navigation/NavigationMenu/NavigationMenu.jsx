e09bea99ae275fc4ccfc84bf4ebd0e334014f6f2
Ada Loveless <xhorj@protonmail.com>
2022-07-06 14:26:09 +0200

    (NavigationMenu) - fix: paths and formatting

--- front/src/components/organisms/navigation/NavigationMenu/NavigationMenu.jsx
+++ front/src/components/organisms/navigation/NavigationMenu/NavigationMenu.jsx
@@ -5,18 +5,16 @@ import MeditationIcon from 'assetsSVG-main/MeditationIcon';
 import ProfilIcone from 'assetsSVG-main/ProfilIcone';
 import NavigationMenuLink from 'components/molecules/navigation/NavigationMenuLink';
 
-
-
 const NavigationMenu = () => {
     const navMenuData = {
         ambiances: {
               title: "Ambiances",
-              path: "/ambiance/:id",
+              path: "/ambiance/",
               icon: < AmbianceIcone color="#F3EDD7" iconeSize="15"/>,    
             },
            respiration: {
               title: "Respiration",
-              path: "/respiration/:id",
+              path: "/respiration/",
               icon: < MeditationIcon  stroke="none" fill="#F3EDD7"  width="15" height="15" />,
             },
             profil: {
@@ -28,10 +26,11 @@ const NavigationMenu = () => {
       
   return (
     <NavigationMenuStyle className="side-navbar-menu">
-          <NavigationMenuLink path={navMenuData.ambiances.path} icon={navMenuData.ambiances.icon} title={navMenuData.ambiances.title}/>
-          <NavigationMenuLink path={navMenuData.respiration.path} icon={navMenuData.respiration.icon} title={navMenuData.respiration.title}/>
-          <NavigationMenuLink path={navMenuData.profil.path} icon={navMenuData.profil.icon} title={navMenuData.ambiances.title}/>
-         </NavigationMenuStyle>
+        <NavigationMenuLink path={navMenuData.ambiances.path} icon={navMenuData.ambiances.icon} title={navMenuData.ambiances.title}/>
+        <NavigationMenuLink path={navMenuData.respiration.path} icon={navMenuData.respiration.icon} title={navMenuData.respiration.title}/>
+        <NavigationMenuLink path={navMenuData.profil.path} icon={navMenuData.profil.icon} title={navMenuData.ambiances.title}/>
+    </NavigationMenuStyle>
   )
 }
+
 export default NavigationMenu
