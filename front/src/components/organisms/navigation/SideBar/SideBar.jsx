7aa442a2874f63223811e3646b60889b2f942de0
Ada Loveless <xhorj@protonmail.com>
2022-07-06 16:21:37 +0200

    refactor: arch to tori icon

--- front/src/components/organisms/navigation/SideBar/SideBar.jsx
+++ front/src/components/organisms/navigation/SideBar/SideBar.jsx
@@ -1,20 +1,18 @@
-import ToriIcon from 'assetsSVG-main/Arch';
-import LogoIcon from 'assetsSVG-main/Logo';
-import React from 'react'  ; 
-import "./Sidebar.css" ;
-import NavigationMenu from 'components/organisms/navigation/NavigationMenu/NavigationMenu';
-import SideBarStyle from './SideBar.style';
-
+import ToriIcon from "components/atoms/embedding/ToriIcon/ToriIcon";
+import LogoIcon from "assetsSVG-main/Logo";
+import React from "react";
+import "./Sidebar.css";
+import NavigationMenu from "components/organisms/navigation/NavigationMenu/NavigationMenu";
+import SideBarStyle from "./SideBar.style";
 
 const SideBar = () => {
-
   return (
     <SideBarStyle>
-        <LogoIcon fill="#F3EDD7"  width="78" height="78" />
-        <NavigationMenu/>
-        <ToriIcon  fill="#F3EDD7" stroke="#F3EDD7" width="70" height="70" />
+      <LogoIcon fill="#F3EDD7" width="78" height="78" />
+      <NavigationMenu />
+      <ToriIcon fill="#F3EDD7" stroke="#F3EDD7" width="70" height="70" />
     </SideBarStyle>
-  )
-}
+  );
+};
 
-export default SideBar ;
+export default SideBar;
