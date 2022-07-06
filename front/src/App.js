8f1f7b3081116e443637e21f4ab05012e71b8974
Ada Loveless <xhorj@protonmail.com>
2022-07-06 14:23:50 +0200

    (App) - refactor: removed pages

--- front/src/App.js
+++ front/src/App.js
@@ -1,38 +1,23 @@
 import "App.css";
 import React from 'react';
 import ThemeProvider from "providers/ThemeProvider";
-//import Text from "components/atoms/grouping/Text/Text";
-//import Button from "components/atoms/forms/Button/Button";
-//import Header from "components/organisms/Header/Header";
-
-// @NOTE - [TODO | Val'] : debug <Header /> for toggle theme
 
 import {
   Routes,
   Route
 } from "react-router-dom";
-import Main from "components/organisms/main/Main";
-import Respiration from "components/pages/respiration/Respiration";
-import Ambiances from "components/pages/ambiances/Ambiances";
-import Profil from "components/pages/profil/Profil";
-import Home from "components/pages/homeCom/Home";
-import SideBar from 'components/organisms/SideBar/SideBar';
-
 
 function App() {
 
   return (
     <ThemeProvider>
       <div className="App">
-     <SideBar></SideBar>
-     <Main >
-           <Routes>
-              < Route path="/"   element={ <Home />} />
-              < Route path="/respiration/:id"    element={ < Respiration />}  />
-              < Route path="/ambiance/:id"  element={ < Ambiances /> } />
-              < Route path="/profil"  element={ < Profil /> } />
-           </Routes> 
-     </Main>
+        <Routes>
+          <Route path="/" element={ <div />} />
+          <Route path="/respiration/" element={ < div />}  />
+          <Route path="/ambiance/" element={ < div /> } />
+          <Route path="/profil" element={ < div /> } />
+          </Routes> 
     </div>
     </ThemeProvider>
   );
