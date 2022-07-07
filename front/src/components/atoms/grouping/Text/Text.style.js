725881c50dcfa1fa635da9a4f1e23369dede444d
Ada Loveless <xhorj@protonmail.com>
2022-07-07 16:33:19 +0200

    (TextStyle) - feat: added xsmall size

--- front/src/components/atoms/grouping/Text/Text.style.js
+++ front/src/components/atoms/grouping/Text/Text.style.js
@@ -1,27 +1,30 @@
-import styled, { css } from 'styled-components'
+import styled, { css } from "styled-components";
 
 const TextStyle = styled.p`
-    color: ${({ theme, $color }) => theme.colors[$color]};
-    ${({ $size }) => {
-        let fontSize;
-        switch ($size) {
-            case 'small':
-                fontSize = 20;
-                break;
-            case 'large':
-                fontSize = 36;
-                break;
-            case 'xlarge':
-                fontSize = 48;
-                break;
-            default:
-                fontSize = 24;
-                break;
-        }
-        return css`
-            font-size: ${fontSize}px;
-        `
-    }}
+  color: ${({ theme, $color }) => theme.colors[$color]};
+  ${({ $size }) => {
+    let fontSize;
+    switch ($size) {
+      case "xsmall":
+        fontSize = 16;
+        break;
+      case "small":
+        fontSize = 20;
+        break;
+      case "large":
+        fontSize = 36;
+        break;
+      case "xlarge":
+        fontSize = 48;
+        break;
+      default:
+        fontSize = 24;
+        break;
+    }
+    return css`
+      font-size: ${fontSize}px;
+    `;
+  }}
 `;
 
 export default TextStyle;
