fd1468da19eac74966aa7bf7102f7c50077110c9
Ada Loveless <xhorj@protonmail.com>
2022-07-07 14:08:17 +0200

    (ColorEntity) - feat: added services folder and entity for color api

---
+++ front/src/services/Entities/ColorEntity.js
@@ -0,0 +1,14 @@
+import { axiosInstance } from "services/axiosConfiguration";
+
+export default class ColorEntity {
+    
+    /**
+     * 
+     * @param {Object} data { colorHexadecimal : <RGB Code> }
+     * @return Axios Instance
+     */
+    static async postColor(data) {
+        return await axiosInstance.get(`/color`, data)
+    }
+  }
+  
