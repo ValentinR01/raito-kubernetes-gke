bf468a1fe40dd23066977a79f89a9df4e55457e3
Ada Loveless <xhorj@protonmail.com>
2022-07-07 14:41:34 +0200

    (ColorField) - feat: added onChange prop

--- front/src/components/molecules/forms/ColorField/ColorField.jsx
+++ front/src/components/molecules/forms/ColorField/ColorField.jsx
@@ -3,12 +3,12 @@ import React from "react";
 import ColorFieldStyle from "./ColorField.style";
 import Stack from "components/atoms/grouping/Stack/Stack";
 
-const ColorField = ({ label }) => {
+const ColorField = ({ label, onChange }) => {
   return (
     <ColorFieldStyle>
       <Stack>
         <Label>{label}</Label>
-        <input type="color" id="color" name="color" />
+        <input type="color" id="color" name="color" onChange={onChange} />
       </Stack>
     </ColorFieldStyle>
   );
