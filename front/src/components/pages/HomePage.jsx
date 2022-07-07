c98b49446d930a30a2b0665e0516a7dc68050f75
TaniaMAHOUCHE <mahouchetania15@gmail.com>
2022-07-07 11:35:50 +0200

    Added correct routes

--- front/src/components/pages/HomePage.jsx
+++ front/src/components/pages/HomePage.jsx
@@ -1,6 +1,7 @@
+import { Text } from "components/atoms/grouping";
 import Card from "components/atoms/grouping/Card/Card";
 import Heading from "components/atoms/text/Heading/Heading";
-import PageTemplate from "components/templates/PageTemplate";
+import PageTemplate from "components/templates/PageTemplate/PageTemplate";
 import React from "react";
 
 const HomePage = () => {
@@ -8,6 +9,7 @@ const HomePage = () => {
     <PageTemplate>
       <Card medium>
       <Heading size="xlarge" color="primary">RAITŌ ライト</Heading>
+        <Text>Avec Raito, nous vous proposons un outil complet de bien être et d’épanouissement via la musique, la lumière et la respiration. Notre objectif est de vous offrir une bulle, pour prendre soin de vous.Nous vous proposons d’utiliser Raito aux moments clés de la journée que sont le réveil et l’endormissement, ainsi que dans les moments où vous souhaitez lâcher prise et vous détendre.</Text>
       </Card>
     </PageTemplate>
   );
