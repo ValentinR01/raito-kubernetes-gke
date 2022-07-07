fa3c3f8502f48bfada47738e10279ff79112ed16
TaniaMAHOUCHE <mahouchetania15@gmail.com>
2022-07-07 16:38:19 +0200

    Fix: css logo

--- front/src/components/organisms/navigation/SideBar/SideBar.jsx
+++ front/src/components/organisms/navigation/SideBar/SideBar.jsx
@@ -1,13 +1,12 @@
 import ToriIcon from "components/atoms/embedding/ToriIcon/ToriIcon";
 import LogoIcon from "components/atoms/embedding/LogoIcon/LogoIcon";
-// import "./Sidebar.css";
 import NavigationMenu from "components/organisms/navigation/NavigationMenu/NavigationMenu";
 import SideBarStyle from "./SideBar.style";
 
 const SideBar = () => {
   return (
     <SideBarStyle>
-      <LogoIcon fill="#F3EDD7" width="83" height="83" /> 
+      <LogoIcon fill="#F3EDD7" width="90" height="90" /> 
       <NavigationMenu />
       <ToriIcon fill="#F3EDD7" stroke="#F3EDD7" width="140" height="140" />
     </SideBarStyle>
