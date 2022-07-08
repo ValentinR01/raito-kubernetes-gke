40014f86e84933b7d9ae6e766c5f995d17f9d79c
TaniaMAHOUCHE <mahouchetania15@gmail.com>
2022-07-08 12:48:38 +0200

    Fix: Css Button Modal Style

---
+++ front/src/components/atoms/grouping/ModalBackground/ModalBackground.style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const ModalBackgroundStyle = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0,0,0,0.3);
+    z-index: 10;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+export default ModalBackgroundStyle;
---
+++ front/src/components/organisms/LoginForm/CloseButtonModalStyle.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const CloseButtonModalStyle = styled.button`
+    border: none; 
+    background: transparent;
+    margin-left: 550px;
+`;
+// transform: 'translate(-50%, -50%);
+export default CloseButtonModalStyle;
