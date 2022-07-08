9cbbc69ccf49dd8d42a6c9f5a88175e06dd78f10
Ada Loveless <xhorj@protonmail.com>
2022-07-08 16:45:47 +0200

    (Input) - refactored: grouped all inputs

---
+++ front/src/components/atoms/forms/Input/InputStyle.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const InputEmailStyle = styled.input`
+  border: none;
+  background-color: #fffaef;
+  border-radius: 20px;
+  width: 275px;
+  padding: 0.35em 1em;
+  font-size: 16px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+export default InputEmailStyle;
+//  padding: 0.35em 1em;
