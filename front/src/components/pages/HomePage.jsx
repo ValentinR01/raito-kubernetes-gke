3b204d2299a5b095a47f273fddfa2dd3f738a45f
Ada Loveless <xhorj@protonmail.com>
2022-07-08 12:48:28 +0200

    (HomePage) - refactor: comments and prettier

--- front/src/components/pages/HomePage.jsx
+++ front/src/components/pages/HomePage.jsx
@@ -8,17 +8,18 @@ const HomePage = () => {
   return (
     <PageTemplate>
       <Card medium>
-      <Heading size="xlarge" color="primary">RAITŌ ライト</Heading>
-        <Text color="black">Avec Raito, nous vous proposons un outil complet de bien être et d’épanouissement via 
-          la musique, la lumière et la respiration. 
+        <Heading size="xlarge" color="primary">
+          RAITŌ ライト
+        </Heading>
+        <Text color="black">
+          Avec Raito, nous vous proposons un outil complet de bien être et
+          d’épanouissement via la musique, la lumière et la respiration.
         </Text>
         <Text color="black">
-          Notre objectif est de vous offrir une bulle, pour prendre soin de vous.
-        {/* </Text>
-        <Text> */}
-          Nous vous proposons d’utiliser Raito aux moments clés de la journée que 
-          sont le réveil et l’endormissement, ainsi que dans les moments où vous souhaitez lâcher prise 
-          et vous détendre.
+          Notre objectif est de vous offrir une bulle, pour prendre soin de
+          vous. Nous vous proposons d’utiliser Raito aux moments clés de la
+          journée que sont le réveil et l’endormissement, ainsi que dans les
+          moments où vous souhaitez lâcher prise et vous détendre.
         </Text>
       </Card>
     </PageTemplate>
