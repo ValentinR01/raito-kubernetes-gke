83bac6c4fd5460025b04313ac2dfc6b0f77bf0a5
Ada Loveless <xhorj@protonmail.com>
2022-07-09 15:53:41 +0200

    (Ambiances) - fix: reusing components

--- front/src/components/pages/Ambiances/Ambiances.jsx
+++ front/src/components/pages/Ambiances/Ambiances.jsx
@@ -1,57 +1,45 @@
-import PageContentTemplate from 'components/templates/PageContentTemplate/PageContentTemplate';
-import React  from 'react'
+import PageContentTemplate from "components/templates/PageContentTemplate/PageContentTemplate";
+import React from "react";
 
-import AmbiancesStyle from './Ambiances.style';
-import { ambiancesList } from './DataAmbiances';
-import { useNavigate } from 'react-router';
+import AmbiancesStyle from "./Ambiances.style";
+import { ambiancesList } from "./DataAmbiances";
+import { useNavigate } from "react-router";
+import { Card } from "components/atoms/grouping";
+import { Heading } from "components/atoms/text";
 
 const Ambiances = () => {
-
-
-
-
-
   const navigate = useNavigate();
   const handleClick = (props) => {
     navigate(`/step2/${props}`, { replace: true });
   };
 
-  const ShowAllAmbianceData =  ambiancesList.map((element) => {
-
+  const ShowAllAmbianceData = ambiancesList.map((element) => {
     return (
-            <div  className="card-body" key={element.id} onClick={() => handleClick(element.id)} >
-                <div className="card-illustration">
-                {element.illustration}
-
-                </div>
-                <div className="text-and-icon">
-                   <div class="card-text-area">
-                       <h5  className="card-title">{element.title}</h5>              
-                       <p  className="card-text">{element.author}</p>
-                    </div>
-                    <div className="card-icon"> {element.icon}  </div>
-                </div>
-            </div>
-         
-    )
-  })
-
+      <div
+        className="card-body"
+        key={element.id}
+        onClick={() => handleClick(element.id)}
+      >
+        <div className="card-illustration">{element.illustration}</div>
+        <div className="text-and-icon">
+          <div class="card-text-area">
+            <h5 className="card-title">{element.title}</h5>
+            <p className="card-text">{element.author}</p>
+          </div>
+          <div className="card-icon"> {element.icon} </div>
+        </div>
+      </div>
+    );
+  });
 
   return (
-      <AmbiancesStyle>
-          <h1>Ambiances</h1>
-          <PageContentTemplate>
+    <PageContentTemplate>
+      <Card>
+        <Heading>Ambiances</Heading>
+        {/* <div className="all-cards">{ShowAllAmbianceData}</div> */}
+      </Card>
+    </PageContentTemplate>
+  );
+};
 
-
-          <div className="all-cards" >
-          {ShowAllAmbianceData}
-          </div>  
-          </PageContentTemplate>
-
-      </AmbiancesStyle>
-    
-  
-  )
-}
-
-export default Ambiances ;
+export default Ambiances;
