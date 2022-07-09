c86c07af6b5ff832a3209728fc01bf5643d65d87
Ada Loveless <xhorj@protonmail.com>
2022-07-09 15:53:18 +0200

    (App.js) - fix: removed Page Template

--- front/src/App.js
+++ front/src/App.js
@@ -1,36 +1,30 @@
 import "App.css";
 import React from "react";
 import ThemeProvider from "providers/ThemeProvider";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AppBackground from "components/molecules/embedding/AppBackground/AppBackground";
-import PageTemplate from "components/templates/PageTemplate/PageTemplate";
 import Ambiances from "components/pages/Ambiances/Ambiances";
 import AmbianceSinglePage from "components/pages/AmbianceSinglePage/AmbianceSinglePage";
-import Step2 from "components/pages/Step2/Step2" ;
+import Step2 from "components/pages/Step2/Step2";
 import HomePage from "components/pages/HomePage";
 
 function App() {
   return (
     <ThemeProvider>
       <div className="App">
-        <PageTemplate>
         <Routes>
-          <Route path="/" element={< HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/respiration/" element={<div />} />
           <Route path="/profil" element={<div />} />
-          <Route path="/ambiances"  element={<Ambiances />} />
+          <Route path="/ambiances" element={<Ambiances />} />
           <Route path="/step2" element={<Step2 />}>
             <Route path=":ambianceId" element={<AmbianceSinglePage />} />
           </Route>
         </Routes>
-        </PageTemplate>
-        
-        
         <AppBackground />
       </div>
     </ThemeProvider>
-
   );
 }
 
-export default App ;
+export default App;
