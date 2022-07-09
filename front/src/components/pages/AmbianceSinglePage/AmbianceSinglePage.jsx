897111ed59d3639daf0ed11be0441e92f8aab6d6
Ada Loveless <xhorj@protonmail.com>
2022-07-09 16:24:11 +0200

    AmbianceSinglePage - fix: formatting and added page template and card

--- front/src/components/pages/AmbianceSinglePage/AmbianceSinglePage.jsx
+++ front/src/components/pages/AmbianceSinglePage/AmbianceSinglePage.jsx
@@ -1,83 +1,94 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useParams } from 'react-router';
-import { ambiancesList } from '../Ambiances/DataAmbiances';
-import { songsdata } from '../Ambiances/Player/audio';
-import Player from '../Ambiances/Player/Player';
-import "./Ambiance-style.css" ;
-
+import { Card } from "components/atoms/grouping";
+import { PageTemplate } from "components/templates";
+import React, { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router";
+import { ambiancesList } from "../Ambiances/DataAmbiances";
+import { songsdata } from "../Ambiances/Player/audio";
+import Player from "../Ambiances/Player/Player";
+import "./Ambiance-style.css";
 
 const AmbianceSinglePage = () => {
-    let { ambianceId } = useParams();
-    const [ element , setElement] = useState([...ambiancesList]) ;
-    const [songs, setSongs] = useState(songsdata);
-    const [isplaying, setisplaying] = useState(false);
-    const [currentSong, setCurrentSong] = useState(songsdata[ ambianceId -1 ]);
+  let { ambianceId } = useParams();
+  const [element, setElement] = useState([...ambiancesList]);
+  const [songs, setSongs] = useState(songsdata);
+  const [isplaying, setisplaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(songsdata[ambianceId - 1]);
 
   const audioElem = useRef();
 
-  
   useEffect(() => {
     if (isplaying) {
       audioElem.current.play();
-    }
-    else {
+    } else {
       audioElem.current.pause();
     }
-  }, [isplaying])
+  }, [isplaying]);
 
   const onPlaying = () => {
     const duration = audioElem.current.duration;
     const ct = audioElem.current.currentTime;
-    setCurrentSong({ ...currentSong, "progress": ct / duration * 100, "length": duration })
-  }
+    setCurrentSong({
+      ...currentSong,
+      progress: (ct / duration) * 100,
+      length: duration,
+    });
+  };
 
+  let theSong;
 
-  let theSong ;
+  Object.values(element).forEach((e) => {
+    let x = parseInt(ambianceId);
+    if (e.id === x) {
+      theSong = e;
+    }
+  });
+  return (
+    <PageTemplate>
+      <Card>
+        <div className="container-single-ambiance">
+          <div className="mini-illu">
+            <div className="color-area"> </div>
 
-  Object.values( element).forEach(e => {
-      let x = parseInt(ambianceId)
-      if( e.id === x ){
-         theSong =  e ;
-      } 
-  })
-    return (
-    
-        
-        <div className="container-single-ambiance" >
-                <div className="mini-illu"  > 
-                    <div className="color-area">   </div>
-                   
-                   <div className="content"> 
-                      <div>
-                        <h3>{theSong.title} </h3>
-                        <p> <strong>Auteur :</strong>   {theSong.author} </p>
-                      </div>
-                   </div>
-                 
-                </div>
-                <div className="description" > 
-                        <div> 
-                          <h4> Qu'est-ce que c'est ?  </h4>
-                          <p>{theSong.description} </p>  
-                        </div> 
-                        
-                </div>
-                <div className="play-area" >
-                    
-                          
-
-                    <div className="play-illustration"> {theSong.illustration} </div>
-                    <div className="play-zone">
-                          <audio src={currentSong.url} ref={audioElem} onTimeUpdate={onPlaying} />
-                          <Player songs={songs} setSongs={setSongs} isplaying={isplaying} setisplaying={setisplaying} audioElem={audioElem} currentSong={currentSong} setCurrentSong={setCurrentSong} />
-                    </div>
-                <div>
+            <div className="content">
+              <div>
+                <h3>{theSong.title} </h3>
+                <p>
+                  {" "}
+                  <strong>Auteur :</strong> {theSong.author}{" "}
+                </p>
               </div>
-              </div>
-      </div>
-    
-    );
-  }
+            </div>
+          </div>
+          <div className="description">
+            <div>
+              <h4> Qu'est-ce que c'est ? </h4>
+              <p>{theSong.description} </p>
+            </div>
+          </div>
+          <div className="play-area">
+            <div className="play-illustration"> {theSong.illustration} </div>
+            <div className="play-zone">
+              <audio
+                src={currentSong.url}
+                ref={audioElem}
+                onTimeUpdate={onPlaying}
+              />
+              <Player
+                songs={songs}
+                setSongs={setSongs}
+                isplaying={isplaying}
+                setisplaying={setisplaying}
+                audioElem={audioElem}
+                currentSong={currentSong}
+                setCurrentSong={setCurrentSong}
+              />
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </Card>
+    </PageTemplate>
+  );
+};
 
-
-export default AmbianceSinglePage ;
+export default AmbianceSinglePage;
