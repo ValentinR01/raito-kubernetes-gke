281fdb9dcd23a4fb9e8ea5dc3dd119f44595a819
Ada Loveless <xhorj@protonmail.com>
2022-07-09 16:24:43 +0200

    (PageTemplate) - fix: removing div and formatting

--- front/src/components/templates/PageTemplate/PageTemplate.jsx
+++ front/src/components/templates/PageTemplate/PageTemplate.jsx
@@ -1,20 +1,15 @@
-import React from 'react';
-import SideBar from 'components/organisms/navigation/SideBar/SideBar';
-import ThemeLoginButtons from 'components/molecules/clickable/ThemeLoginButtons/ThemeLoginButtons';
-import PageTemplateStyle from './PageTemplate.style';
-import PageContentTemplate from 'components/templates/PageContentTemplate/PageContentTemplate';
-import "./style.css" ;
+import React from "react";
+import SideBar from "components/organisms/navigation/SideBar/SideBar";
+import ThemeLoginButtons from "components/molecules/clickable/ThemeLoginButtons/ThemeLoginButtons";
+import PageTemplateStyle from "./PageTemplate.style";
+import PageContentTemplate from "components/templates/PageContentTemplate/PageContentTemplate";
+import "./style.css";
 const PageTemplate = ({ children }) => {
   return (
     <PageTemplateStyle>
       <SideBar />
-      <ThemeLoginButtons/>
-      <div className="page-template">
-      <PageContentTemplate >
-        {children}
-      </PageContentTemplate>      
-      </div>
-     
+      <ThemeLoginButtons />
+      <PageContentTemplate>{children}</PageContentTemplate>
     </PageTemplateStyle>
   );
 };
