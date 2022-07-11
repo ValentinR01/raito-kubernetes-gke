8cc218bc630b92f1bf165d738177d982b8acaf2c
Ada Loveless <xhorj@protonmail.com>
2022-07-11 19:42:08 +0200

    (Profil) - fix: added message when empty

--- front/src/components/pages/Profil/Profil.jsx
+++ front/src/components/pages/Profil/Profil.jsx
@@ -1,16 +1,17 @@
-import { PageTemplate } from 'components/templates';
-import React, { useEffect, useState } from 'react';
-import "./Profil.css" ;
+import { Card, Text } from "components/atoms/grouping";
+import { PageTemplate } from "components/templates";
+import React, { useEffect, useState } from "react";
+import "./Profil.css";
 
 const Profil = () => {
-
   const [userData, setUserData] = useState(null);
+  const [userDataIsLoading, setUserDataIsLoading] = useState(true);
   // const [userData, setUserData] = useState([]);
 
   const [errors, setErrors] = useState(null);
 
-
   const getUser = async () => {
+    setUserDataIsLoading(true);
     try {
       const response = await fetch("http://localhost:2345/user", {
         method: "GET",
@@ -18,63 +19,74 @@ const Profil = () => {
         credentials: "include",
       });
       const data = await response.json();
-        console.log(data);
-        setUserData(data);
-  
-  
-      
-  
-    } catch (err){
+      console.log(data);
+      setUserData(data);
+    } catch (err) {
       console.log(err);
+    } finally {
+      setUserDataIsLoading(false);
     }
-  
-  }
-  
-  useEffect(() => {
+  };
 
-    if ( userData) {
-      return  ;
+  useEffect(() => {
+    if (userData) {
+      return;
     }
-    getUser()
-    
-  },[userData])
+    getUser();
+  }, [userData]);
 
   // console.log(userData) ;
 
   const getUserRole = () => {
     if (userData?.role) {
-    switch (userData.role) {
-      case "User" :
-        return "Standard"
-      default :
-        return ;
-     }  
+      switch (userData.role) {
+        case "User":
+          return "Standard";
+        default:
+          return;
+      }
     }
+  };
+
+  if (!userDataIsLoading && !userData) {
+    return (
+      <PageTemplate>
+        <Card>
+          <Text>Connectez-vous pour accéder au profil</Text>
+        </Card>
+      </PageTemplate>
+    );
   }
-
   return (
-     <PageTemplate>
-            <div className="profil-component">
-                <div className="profil-container">
-                    <h3> Espace Personnel</h3>
-                    <div>
-                       <p> <strong> Email</strong></p>
-                       <p> {userData?.email}</p>
-                    </div>
-                    <div>
-                       <p> <strong>Type d'abonnement </strong></p>
-                       <p> {getUserRole()}</p>
+    <PageTemplate>
+      <div className="profil-component">
+        <div className="profil-container">
+          <h3> Espace Personnel</h3>
+          <div>
+            <p>
+              {" "}
+              <strong> Email</strong>
+            </p>
+            <p> {userData?.email}</p>
+          </div>
+          <div>
+            <p>
+              {" "}
+              <strong>Type d'abonnement </strong>
+            </p>
+            <p> {getUserRole()}</p>
+          </div>
+          <div>
+            <p>
+              {" "}
+              <strong>Date d'inscription </strong>
+            </p>
+            <p> {userData?.date_inscription}</p>
+          </div>
+        </div>
+      </div>
+    </PageTemplate>
+  );
+};
 
-                    </div>
-                    <div>
-                       <p> <strong>Date d'inscription </strong></p>
-                       <p> {userData?.date_inscription}</p>
-                    </div>
-                </div>
-            </div>
-     </PageTemplate>
-
-  )
-}
-
-export default Profil ;
+export default Profil;
