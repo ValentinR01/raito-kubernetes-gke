b525bcdd68f432ce0a72c3098712c8cbfee68ba1
Ada Loveless <xhorj@protonmail.com>
2022-07-11 19:19:15 +0200

    (Modal) - refactor: imports and reformat

--- front/src/components/organisms/LoginForm/Modal.jsx
+++ front/src/components/organisms/LoginForm/Modal.jsx
@@ -1,25 +1,26 @@
-import React, { Fragment } from 'react';
-import ModalStyle from './ModalStyle.style'
-import LoginForm from '../../molecules/forms/LoginForm/LoginForm'
-import CloseButtonModalStyle from './CloseButtonModalStyle';
-import Card from 'components/atoms/grouping/Card/Card';
-import ModalBackground from 'components/atoms/grouping/ModalBackground/ModalBackground';
+import React from "react";
+import ModalStyle from "./ModalStyle.style";
+import LoginForm from "../../molecules/forms/LoginForm/LoginForm";
+import CloseButtonModalStyle from "./CloseButtonModalStyle";
+import Card from "components/atoms/grouping/Card/Card";
+import ModalBackground from "components/atoms/grouping/ModalBackground/ModalBackground";
 
-const Modal = ({open, children, onClose}) => {
-    if (!open) return null
+const Modal = ({ open, children, onClose }) => {
+  if (!open) return null;
 
-    return (  
-            <ModalStyle>
-                <ModalBackground>
-                    <Card medium>
-                        {/* <button onClick={onClose}>close;</button> */}
-                        <CloseButtonModalStyle onClick={onClose}>&times;</CloseButtonModalStyle>
-                        {children}
-                        <LoginForm />
-                    </Card>
-                </ModalBackground>
-            </ModalStyle>
-    )
-}
-  
+  return (
+    <ModalStyle>
+      <ModalBackground>
+        <Card medium>
+          <CloseButtonModalStyle onClick={onClose}>
+            &times;
+          </CloseButtonModalStyle>
+          {children}
+          <LoginForm />
+        </Card>
+      </ModalBackground>
+    </ModalStyle>
+  );
+};
+
 export default Modal;
