eb95bb49b7f2e4e24b7cbfdb0a67adad6755e136
lestox <78364535+lestox@users.noreply.github.com>
2022-07-11 19:19:37 +0200

    ajout script lumière

--- app/src/Controller/ColorController.php
+++ app/src/Controller/ColorController.php
@@ -13,7 +13,12 @@ class ColorController extends BaseController
         if ($this->HTTPRequest->isMethodAllowed('POST')) {
             $response = $this->HTTPRequest->getColor();
             if (is_string($response)) {
+                $response = str_replace("#", "0x", $response);
                 $this->renderJSON($response);
+                // Envoi rgb à led à ce niveau
+                $this->HTTPRequest->sendColor($response);
+                //$colorRGB = $this->hexToRgb($response);
+                //$this->renderJSON($colorRGB);
             } else {
                 //Récupération des valeurs
                 $colorHexadecimal = $response['colorHexadecimal'];
@@ -28,4 +33,16 @@ class ColorController extends BaseController
             }
         }
     }
+
+//    function hexToRgb($hex, $alpha = false) {
+//        $hex      = str_replace('#', '', $hex);
+//        $length   = strlen($hex);
+//        $rgb['r'] = hexdec($length == 6 ? substr($hex, 0, 2) : ($length == 3 ? str_repeat(substr($hex, 0, 1), 2) : 0));
+//        $rgb['g'] = hexdec($length == 6 ? substr($hex, 2, 2) : ($length == 3 ? str_repeat(substr($hex, 1, 1), 2) : 0));
+//        $rgb['b'] = hexdec($length == 6 ? substr($hex, 4, 2) : ($length == 3 ? str_repeat(substr($hex, 2, 1), 2) : 0));
+//        if ( $alpha ) {
+//            $rgb['a'] = $alpha;
+//        }
+//        return $rgb;
+//    }
 }
