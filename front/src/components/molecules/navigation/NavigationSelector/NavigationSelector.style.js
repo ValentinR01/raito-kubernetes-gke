1d25402e45c0b629856406f60e52f9327931f886
Ada Loveless <xhorj@protonmail.com>
2022-07-11 17:22:00 +0200

    (NavigationSelector.style) - fix: finished styling selector

--- front/src/components/molecules/navigation/NavigationSelector/NavigationSelector.style.js
+++ front/src/components/molecules/navigation/NavigationSelector/NavigationSelector.style.js
@@ -1,6 +1,9 @@
 import styled, { css } from "styled-components";
 
 const NavigationMenuLinkStyle = styled.span`
+  position: absolute;
+  left: 0px;
+  margin-top: -15px;
   ${({ $visible }) => {
     let visibility = "hidden";
     if ($visible) {
